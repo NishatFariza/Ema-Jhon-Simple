e36bc7a450b613404321966a99bd528eb6883c51
NishatFariza <nishatfarzia78@gmail.com>
2022-03-23 19:40:30 +0600

    navbar add

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Header from "./components/Header/Header";
 
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from '../../images/Logo.svg';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <nav className='header-nav'>
+            <img src={logo} />
+            <div>
+                <a href="/shop">Shop</a>
+                <a href="/order">Order</a>
+                <a href="/inventory">Inventory</a>
+                <a href="/about">About</a>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
