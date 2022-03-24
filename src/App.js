a2869f9a97276692a6c93e1164bfbce7d42a10db
NishatFariza <nishatfarzia78@gmail.com>
2022-03-25 02:53:48 +0600

    data load

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Header/Header";
+import Shop from "./components/Shop/Shop";
 
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Shop></Shop>
     </div>
   );
 }
