f727a72b99d2f01247084152c2176592aabc68b5
NishatFariza <nishatfarzia78@gmail.com>
2022-04-03 12:14:54 +0600

    route add

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Shop from "./components/Shop/Shop";
 
@@ -6,6 +7,9 @@ function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Routes>
+        <Route></Route>
+      </Routes>
       <Shop></Shop>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </BrowserRouter>,
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
